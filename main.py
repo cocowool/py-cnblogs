033f0b19c8518345e2791325be7a6d9132edee2e
Green Wang <cocowool@gmail.com>
2020-03-16 19:25:10 +0800

    modify main function

--- main.py
+++ main.py
@@ -3,5 +3,23 @@
 # Author: Wang Shiqiang
 import requests
 
-response = requests.get("https://www.baidu.com")
-print(response.text)
+# 入口函数，输入文章列表页地址后进行文件抓取
+def main(argv):
+    try :
+        print ("Hello")
+        opts,args = getopt.getopt(argv,"hl:",["link="])
+        print(args)
+    except getopt.GetoptError:
+        print("Usage: python3 main.py -l <blog_post_list_link>")
+
+    for opt, arg in opts:
+        if opt in ("-l", "--link"):
+            parse_list(arg)
+            pass
+        else:
+            print("Usage: python3 main.py -l <blog_post_list_link>")
+
+
+def parse_list(url):
+    response = requests.get("https://www.baidu.com")
+    print(response.text)
