95d02bb5502af4dc4aa6386521cf266ec33eb374
Green Wang <cocowool@gmail.com>
2020-03-16 22:56:49 +0800

    get post list

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import os, sys, getopt
 import re
 import requests
+from bs4 import BeautifulSoup
 
 # 入口函数，输入文章列表页地址后进行文件抓取
 def main(argv):
@@ -22,13 +23,20 @@ def main(argv):
 def parse_list(url):
     html = get_html(url)
 
-    print(html)
+    soup = bs4_parse(html)
 
-
+# 通过requests方式获取网页内容
 def get_html(url, method = "requests"):
     response = requests.get(url)
 
     return response.text
+
+# 使用BeautifulSoup解析HTML
+def bs4_parse(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    post_lists = soup.find_all('div', attrs={'class':'day'})
+    print(post_lists)
 
 if __name__ == "__main__":
     # print(__name__)
