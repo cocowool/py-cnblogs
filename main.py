a7980d5788db1ed0593d9a88d013e629e4a496f8
Green Wang <cocowool@gmail.com>
2020-03-16 20:03:26 +0800

    import module

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # 抓取博客园或其他博客的文章
 #
 # Author: Wang Shiqiang
+import os, sys, getopt
 import requests
 
 # 入口函数，输入文章列表页地址后进行文件抓取
 def main(argv):
     try :
-        print ("Hello")
+        print("hello")
         opts,args = getopt.getopt(argv,"hl:",["link="])
-        print(args)
     except getopt.GetoptError:
         print("Usage: python3 main.py -l <blog_post_list_link>")
 
@@ -23,3 +23,8 @@ def main(argv):
 def parse_list(url):
     response = requests.get("https://www.baidu.com")
     print(response.text)
+
+
+if __name__ == "__main__":
+    # print(__name__)
+    main(sys.argv[1:])
