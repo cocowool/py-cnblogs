73f0ab923871f3261b0298c2aec60eac96713d29
Green Wang <cocowool@gmail.com>
2020-03-30 14:35:03 +0800

    tranverse the dom

---
+++ blog2markdown/__main__.py
@@ -0,0 +1,5 @@
+def main():
+    print("html2markdown Main()")
+
+if __name__ == '__main__':
+    main()
--- blog2markdown/html2markdown.py
+++ blog2markdown/html2markdown.py
@@ -8,12 +8,20 @@
 # (一)列出需要转换的元素，从HTML中搜索并替换相关元素
 # (二)遍历DOM树，依次将遇到的元素做替换
 
+from bs4 import BeautifulSoup
 
 class html2markdown():
     def __init__(self):    
         print("Html to Markdown module")
 
     def convert(self, html_string):
+        soup = BeautifulSoup(html_string, 'html.parser')
+
+        if not soup.contents :
+            return soup.get_text()
+
+        
+        print(soup.contents)
         pass
 
     def convertFile():
