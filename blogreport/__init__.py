a5e5d6c6831ac16a08ef76fa50fc331aa4c1c9f5
Shiqiang <cocowool@qq.com>
2020-12-30 19:12:04 +0800

    init cnblog report project

---
+++ blogreport/__init__.py
@@ -0,0 +1,14 @@
+# 支持生成博客园的年度报告
+# Author: Wang Shiqiang
+# Date: 2020/12/30
+
+__author__ = "Wang Shiqiang"
+__version__ = "0.0.1"
+
+from bs4 import BeautifulSoup
+import re
+import sys
+
+from .html2markdown import html2markdown
+from .blog2html import blog2html
+
---
+++ report.py
@@ -0,0 +1,14 @@
+from blog2markdown import html2markdown
+from blog2markdown import blog2html
+
+# 将博客文章和博客用到的图片保存到本地，文章保存为html格式，图片保存到markdown文件夹下
+# 输入博客的入口，则自动将所有博客文章下载到本地的 cnblogs-{blog-name}/html 文件夹下
+b2h = blog2html()
+b2h.get_cnblogs('https://www.cnblogs.com/ityouknow/')
+
+h2m = html2markdown()
+# Conver Folder
+# 将文件夹下的html文件批量转换为markdown文件
+# h2m.convertFolder('/Users/shiqiang/Projects/py-cnblogs/cnblogs-coco1s/htmls', '/Users/shiqiang/Projects/py-cnblogs/cnblogs-coco1s/markdowns')
+
+# 支持自动抓取博客园文章，并生成Markdown格式的年度报告
